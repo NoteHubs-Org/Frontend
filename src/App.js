80a266d629e8a27dd78b40b3576c3c5c0892660a
Ali Masoud <alimasoud827@gmail.com>
2025-02-13 19:42:13 +0300

    initial commit header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './header/Header';
+import LandPage from './landingPage/LandPage';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <LandPage />
+    </>
+  )
+}
+
+export default App;
