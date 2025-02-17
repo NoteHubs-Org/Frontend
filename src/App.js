bc5d31e653b10dd175c84a7654049f08422d8a5d
Ali Masoud <alimasoud827@gmail.com>
2025-02-17 16:53:29 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './header/Header';
 import LandPage from './landingPage/LandPage';
 import Footer from './footer/Footer';
+import { BrowserRouter } from 'react-router-dom'; 
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -11,12 +12,20 @@ function App() {
     setIsVisible(!isVisible);
   };
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <>
+    <BrowserRouter>
       <Header toggleProfile={toggleProfile} />
-      <LandPage isProfileVisible={isVisible} toggleProfile={toggleProfile} />
+      <LandPage isProfileVisible={isVisible} toggleProfile={toggleProfile}
+      toggleSidebar={toggleSidebar}
+      isExpanded={isExpanded} />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/landingPage/LandPage.js
+++ src/landingPage/LandPage.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Sidebar from "./sidebar/Sidebar";
 import Profile from "../header/Profile";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"; // Remove Router from here
 import Dashboard from "./L_Body/Dashboard";
 import UploadPage from "../uploads/UploadPage";
 
-const LandPage = ({ isProfileVisible, toggleProfile }) => {
+const LandPage = ({ isProfileVisible, toggleProfile, toggleSidebar, isExpanded }) => {
   return (
     <>
-      <Sidebar className="sidebar" />
+      <Sidebar toggleSidebar={toggleSidebar} 
+      isExpanded={isExpanded}
+      className="sidebar" />
       <Profile isOpen={isProfileVisible} toggleProfile={toggleProfile} />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/summarize" element={<UploadPage />} />
-          <Route path="/NoteAi" element={<Dashboard />} />
-          <Route path="/groups" element={<Dashboard />} />
-          <Route path="/QAs" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/summarize" element={<UploadPage />} />
+        <Route path="/NoteAi" element={<Dashboard />} />
+        <Route path="/groups" element={<Dashboard />} />
+        <Route path="/QAs" element={<Dashboard />} />
+      </Routes>
     </>
   );
 };
