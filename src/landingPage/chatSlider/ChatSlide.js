98bc891ae9779e9b6cf4e062a202cd0bc561b5c6
Ali Masoud <alimasoud827@gmail.com>
2025-06-01 15:10:22 +0300

    chatslider done

--- src/landingPage/chatSlider/ChatSlide.js
+++ src/landingPage/chatSlider/ChatSlide.js
@@ -1,4 +1,4 @@
-import React, { useEffect  } from "react";
+import React, { useEffect, useState  } from "react";
 import "./chatslider.css";
 // import { PiXBold } from "react-icons/pi";
 // import { MdOutlineGroups2 } from "react-icons/md";
@@ -8,6 +8,27 @@ import "./chatslider.css";
 
 const ChatSlide = ({ isVisible, toggleChat }) => {
   // const sidebarRef = useRef(null);
+  const [activeGroupIndex, setActiveGroupIndex] = useState(null);
+  const groups = [
+    {
+      icon: "🏥",
+      courseName: "Degree Nursing",
+      university: "Mt.Kenya University",
+      notif: 2,
+    },
+    {
+      icon: "💻",
+      courseName: "Comp Science",
+      university: "Mt.Kenya University",
+      notif: 2,
+    },
+    {
+      icon: "🧬",
+      courseName: "Gen Science",
+      university: "Kenyatta University",
+      notif: 2,
+    },
+  ];
   useEffect(() => {
     const chatOverlay = document.getElementById("chatOverlay");
     if (isVisible) {
@@ -34,32 +55,20 @@ const ChatSlide = ({ isVisible, toggleChat }) => {
               </div>
               
               <div className="groups-container">
-                  <div className="group">
-                      <div className="group-icon">🏥</div>
-                      <div className="group-mid">
-                          <div className="course-name">Degree Nursing</div>
-                          <div className="university-name">Mt.Kenya University</div>
-                      </div>
-                      <div className="notif-number">2</div>
-                  </div>
-                  
-                  <div className="group">
-                      <div className="group-icon">💻</div>
-                      <div className="group-mid">
-                          <div className="course-name">Comp Science</div>
-                          <div className="university-name">Mt.Kenya University</div>
-                      </div>
-                      <div className="notif-number">2</div>
-                  </div>
-                  
-                  <div className="group">
-                      <div className="group-icon">🧬</div>
-                      <div className="group-mid">
-                          <div className="course-name">Gen Science</div>
-                          <div className="university-name">Kenyatta University</div>
-                      </div>
-                      <div className="notif-number">2</div>
-                  </div>
+                  {groups.map((group, index) => (
+              <div
+                key={index}
+                className={`group ${activeGroupIndex === index ? "group-active" : ""}`}
+                onClick={() => setActiveGroupIndex(index)}
+              >
+                <div className="group-icon">{group.icon}</div>
+                <div className="group-mid">
+                  <div className="course-name">{group.courseName}</div>
+                  <div className="university-name">{group.university}</div>
+                </div>
+                <div className="notif-number">{group.notif}</div>
+              </div>
+            ))}
               </div>
           </div>
           {/* right chat side */}
@@ -69,7 +78,7 @@ const ChatSlide = ({ isVisible, toggleChat }) => {
                       <div className="chat-right-title">Degree Nursing (BSCN87265)</div>
                       <div className="group-population">100 students | Mt.Kenya University</div>
                   </div>
-                  <button onclick={toggleChat} className="X-remove-btn">✕</button>
+                  <button onClick={toggleChat} className="X-remove-btn">✕</button>
               </div>
               
               <div className="chat-right-mid">
