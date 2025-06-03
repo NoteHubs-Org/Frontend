69c471339c7bfd8952716fab4cbb3e98ca1a0ec5
Ali Masoud <alimasoud827@gmail.com>
2025-06-03 15:36:39 +0300

    fix auth tokens

--- src/authRoutes/authTestRoute.mjs
+++ src/authRoutes/authTestRoute.mjs
@@ -1,11 +1,22 @@
-import axios from "axios";
-const api = axios.create({
-    baseURL: "http://localhost:5000/auth",
-    withCredentials: true,
+// import axios from "axios";
+// const api = axios.create({
+//     baseURL: "http://localhost:5000/auth",
+//     withCredentials: true,
+// })
+// try {
+//     const res = await api.get("/all")    
+//     console.log(res.data)
+// } catch (error) {
+//     console.log(error)
+// }
+fetch('http://localhost:5000/auth', {
+  method: 'GET',
+  credentials: 'include' // again, include credentials
 })
-try {
-    const res = await api.get("/all")    
-    console.log(res.data)
-} catch (error) {
-    console.log(error)
-}
+.then(res => {
+  if (!res.ok) throw new Error('Unauthorized');
+  return res.json();
+})
+.then(data => {
+  console.log('Protected data:', data.status);
+});
